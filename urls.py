97c56809d1328039436d21889062cd51eac947d7
Garlin Gilchrist II <garlin@garlin.org>
2010-05-03 08:01:08 -0700

    + Added more robust URLs to support form GET/POST submissions

--- urls.py
+++ urls.py
@@ -10,42 +10,53 @@ urlpatterns = patterns('myproject.shifd_api.views',
 
 	(r'^get/(?P<sid>\w+)/(?P<format>(json|xml|php))$', 'get'),
 	(r'^get/(?P<sid>\w+)$', 'get'),
+	(r'^get/$', 'get'),
 
 	(r'^getList/(?P<type_id>[1-3])/(?P<state>[0-1])/(?P<format>(json|xml|php))$', 'getList'),
 	(r'^getList/(?P<type_id>[1-3])/(?P<state>[0-1])$', 'getList'),
 	(r'^getList/(?P<type_id>[1-3])$', 'getList'),
+	(r'^getList/$', 'getList'),
 	(r'^getList$', 'getList'),
 
 	(r'^getStats/(?P<sid>\w+)/(?P<format>\w+)$', 'getStats'),
 	(r'^getStats/(?P<sid>\w+)$', 'getStats'),
+	(r'^getStats/$', 'getStats'),
 
 	(r'^search/(?P<keywords>\w+)/(?P<type_id>[1-3])/(?P<state>[0-1])/(?P<format>(json|xml|php))$', 'search'),
 	(r'^search/(?P<keywords>\w+)/(?P<type_id>[1-3])$', 'search'),
-	(r'^search/(?P<keywords>\w+)$', 'search'),
+	(r'^search/(?P<keywords>\w+)$', 'search')
+	(r'^search/$', 'search'),
 
 	(r'^add/(?P<content>\w+)/(?P<type_id>[1-3])/(?P<tag>\w+)/(?P<privacy>(public|private))/(?P<format>(json|xml|php))$', 'add'),
 	(r'^add/(?P<content>\w+)/(?P<type_id>[1-3])/(?P<tag>\w+)/(?P<privacy>(public|private))$', 'add'),
 	(r'^add/(?P<content>\w+)/(?P<type_id>[1-3])/(?P<tag>\w+)$', 'add'),
 	(r'^add/(?P<content>\w+)/(?P<type_id>[1-3])$', 'add'),
 	(r'^add/(?P<content>\w+)$', 'add'),
+	(r'^add/$', 'add'),
 
 	(r'^edit/(?P<sid>\w+)/(?P<content>\w+)/(?P<type_id>[1-3])/(?P<tag>\w+)/(?P<privacy>(public|private))/(?P<format>(json|xml|php))$', 'edit'),
 	(r'^edit/(?P<sid>\w+)/(?P<content>\w+)/(?P<type_id>[1-3])/(?P<tag>\w+)/(?P<privacy>(public|private))$', 'edit'),
 	(r'^edit/(?P<sid>\w+)/(?P<content>\w+)/(?P<type_id>[1-3])/(?P<tag>\w+)$', 'edit'),
 	(r'^edit/(?P<sid>\w+)/(?P<content>\w+)/(?P<type_id>[1-3])$', 'edit'),
 	(r'^edit/(?P<sid>\w+)/(?P<content>\w+)$', 'edit'),
+	(r'^edit/(?P<sid>\w+)$', 'edit'),
+	(r'^edit/$', 'edit'),
 
 	(r'^delete/(?P<sid>\w+)/(?P<format>\w+)$', 'delete'),
 	(r'^delete/(?P<sid>\w+)$', 'delete'),
+	(r'^delete/$', 'delete'),
 
 	(r'^archive/(?P<sid>\w+)/(?P<state>(1))/(?P<format>(json|xml|php))$', 'archive'),
 	(r'^archive/(?P<sid>\w+)$', 'archive'),
+	(r'^archive/$', 'archive'),
 
 	(r'^unarchive/(?P<sid>\w+)/(?P<state>(0))/(?P<format>(json|xml|php))$', 'archive'),
 	(r'^unarchive/(?P<sid>\w+)$', 'archive'),
+	(r'^unarchive/$', 'archive'),
 
 	(r'^getExtraInfo/(?P<content>\w+)/(?P<format>(json|xml|php))$', 'getExtraInfo'),
 	(r'^getExtraInfo/(?P<content>\w+)$', 'getExtraInfo'),
+	(r'^getExtraInfo$', 'getExtraInfo'),
 
 	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
 	# to INSTALLED_APPS to enable admin documentation:
